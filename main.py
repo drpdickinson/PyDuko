ba86f7fbd954a6966d0fd85b411f78fceef49c9f
Patrick <pdickinson@lincoln.ac.uk>
2022-05-04 14:37:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+##########################################################
+# (c) Patrick Dickinson, 2022
+#
+##########################################################
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import sodumap
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    mymap = sodumap.cSoduMap()
+    mymap.Load()
+    mymap.Draw()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
